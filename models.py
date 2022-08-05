c145c4e03248b11d1170a23e916d19ccd9e9786c
Zaman, Shaheer Shaheer <zamans@deshaw.com>
2022-08-05 16:25:22 +0530

    first commit

---
+++ models.py
@@ -0,0 +1,18 @@
+import datetime
+from email.policy import default
+from typing import Optional
+
+from sqlmodel import SQLModel, Field
+
+
+class AuctionItem(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    bidder_id: Optional[int]
+    item_name: str
+    item_description: Optional[str]
+    bid: Optional[float]
+    min_price: float = 0
+    price_step: Optional[int]
+    completed: bool = False
+    start: datetime.datetime = datetime.datetime.now()
+    ends: Optional[datetime.datetime]
